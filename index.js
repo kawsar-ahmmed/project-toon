63afbf820387414756d09022c79c16dd2c6a768c
kawsar-ahmmed <kawsarahmmedr@gmail.com>
2023-06-11 13:14:24 +0600

    Data worked properly

--- index.js
+++ index.js
@@ -6,10 +6,30 @@ var cors = require('cors')
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('This is nodemon worked')
+// users
+const users = [
+    { id: 1, name: 'sabana', email: 'sabana@gmail.com', phone: '01299999922' },
+    { id: 2, name: 'sabnooks', email: 'sabnooks@gmail.com', phone: '01299999922' },
+    { id: 3, name: 'srabonti', email: 'srabonti@gmail.com', phone: '01299999922' },
+    { id: 4, name: 'suchuna', email: 'suchuna@gmail.com', phone: '01299999922' },
+    { id: 5, name: 'kobori', email: 'kobori@gmail.com', phone: '01299999922' },
+    { id: 6, name: 'sabila', email: 'sabila@gmail.com', phone: '01299999922' },
+    { id: 7, name: 'shakira', email: 'shakira@gmail.com', phone: '01299999922' },
+    { id: 8, name: 'Anta', email: 'anta@gmail.com', phone: '08976677' },
+];
+
+app.get('/users', (req, res) => {
+    res.send(users)
+})
+
+app.post('/users', (req, res) => {
+    const user = req.body;
+    user.id = users.length + 1;
+    console.log('request', user);
+    users.push(user);
+    res.send(user);
 })
 
 app.listen(port, () => {
-  console.log('CRUD Work', port)
+    console.log('CRUD Work', port)
 })
